ae7006c1b16a780123b13bf132425d86fee78b49
DzmitryU <dzmitry.urbanovich@arteza.com>
2021-05-21 13:02:41 +0300

    translated templates, lists pages

---
+++ app/Observers/CustomersObserver.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Observers;
+
+use App\Models\Customers;
+use Illuminate\Support\Facades\Auth;
+
+class CustomersObserver
+{
+    /**
+     * Handle the Customers "created" event.
+     *
+     * @param  \App\Models\Customers  $customers
+     * @return void
+     */
+    public function creating(Customers $customers)
+    {
+        $user = Auth::guard('backpack')->user();
+
+        $customers->company_id = $user->id;
+    }
+
+    /**
+     * Handle the Customers "updated" event.
+     *
+     * @param  \App\Models\Customers  $customers
+     * @return void
+     */
+    public function updated(Customers $customers)
+    {
+        //
+    }
+
+    /**
+     * Handle the Customers "deleted" event.
+     *
+     * @param  \App\Models\Customers  $customers
+     * @return void
+     */
+    public function deleted(Customers $customers)
+    {
+        //
+    }
+
+    /**
+     * Handle the Customers "restored" event.
+     *
+     * @param  \App\Models\Customers  $customers
+     * @return void
+     */
+    public function restored(Customers $customers)
+    {
+        //
+    }
+
+    /**
+     * Handle the Customers "force deleted" event.
+     *
+     * @param  \App\Models\Customers  $customers
+     * @return void
+     */
+    public function forceDeleted(Customers $customers)
+    {
+        //
+    }
+}
